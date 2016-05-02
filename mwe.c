9664d9f93f7faf4648f41066b8ad338fa368c1ee
Will Killian <william.killian@gmail.com>
2016-05-02 15:57:36 -0400

    MWE

---
+++ mwe.c
@@ -0,0 +1,25 @@
+const int FV_SIZE = 54;
+
+struct Subtree {
+	float fv[FV_SIZE];
+};
+
+float computeSubtreeKernel (const Subtree* begin1, const Subtree* begin2, int size1, int size2) {
+	float globalSim = 0.0f;
+	int globalCount = 0;
+	for (int i1 = 0; i1 < size1; ++i1) {
+		for (int i2 = 0; i2 < size2; ++i2) {
+			Subtree c1 = begin1 + i1;
+			Subtree c2 = begin2 + i2;
+			float normdiff = 0.0f;
+			for (int i = 0; i < FV_SIZE; ++i) {
+				normdiff += fabs (c1.fv[i] - c2.fv[i]) / fmax (c1.fv[i], c2.fv[i]);
+			}
+			if (normdiff > delta) {
+				globalSim += normdiff;
+				globalCount++;
+			}
+		}
+	}
+	return globalSim / globalCount;
+}
