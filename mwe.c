8eabdf86d01053a71b9f2da262e276848ce91bf9
Will Killian <william.killian@gmail.com>
2016-05-02 16:23:53 -0400

    Fixed kernel dereference bug

--- mwe.c
+++ mwe.c
@@ -6,20 +6,18 @@ struct Subtree {
 
 float computeSubtreeKernel (const Subtree* begin1, const Subtree* begin2, int size1, int size2) {
 	float globalSim = 0.0f;
-	int globalCount = 0;
 	for (int i1 = 0; i1 < size1; ++i1) {
 		for (int i2 = 0; i2 < size2; ++i2) {
-			Subtree c1 = begin1 + i1;
-			Subtree c2 = begin2 + i2;
+			Subtree* c1 = begin1 [i1];
+			Subtree* c2 = begin2 [i2];
 			float normdiff = 0.0f;
 			for (int i = 0; i < FV_SIZE; ++i) {
 				normdiff += fabs (c1.fv[i] - c2.fv[i]) / fmax (c1.fv[i], c2.fv[i]);
 			}
 			if (normdiff > delta) {
 				globalSim += normdiff;
-				globalCount++;
 			}
 		}
 	}
-	return globalSim / globalCount;
+	return globalSim / (size1 * size2);
 }
